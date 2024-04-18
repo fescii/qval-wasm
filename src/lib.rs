aa63f05cba07c695c8c8326358facd4f01708ab1
fescii <isfescii@gmail.com>
2024-04-18 15:12:26 +0300

    Hash: generate unique handle(identities)

--- src/lib.rs
+++ src/lib.rs
@@ -2,18 +2,19 @@ use wasm_bindgen::prelude::*;
 use rand::Rng;
 use sha2::Sha256;
 use hmac::{Hmac, Mac};
-
-extern crate hex; // Add this line to import the hex crate
+// use wasm_bindgen_futures::JsFuture;
+// use wasm_bindgen_futures::JsFuture;
+extern crate hex;
 
 
 #[wasm_bindgen]
-pub fn gen_hash(secrete: &str, section: &str, number: u64) -> String {
+pub async fn gen_hash(secrete: &str, section: &str, key: &str) -> String {
     let random: u64 = rand::thread_rng().gen_range(0..100);
     let secret_and_number = format!("{}-{}", secrete, random);
 
     let mut hmac = Hmac::<Sha256>::new_from_slice(secret_and_number.as_bytes()).expect("Failed to create HMAC");
 
-    hmac.update(number.to_string().as_bytes());
+    hmac.update(key.as_bytes());
 
     let digested_hash = hmac.finalize().into_bytes();
 
